36b2cc3f9d7cac9e1c0badb0c26afa0191f019fd
Samuel Hsia <samchsia@meta.com>
2025-05-14 15:39:33 -0700

    Add copyright

--- madmax/tasks/trace.py
+++ madmax/tasks/trace.py
@@ -1,3 +1,12 @@
+"""
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+
+
 # An individual computation or communication trace
 class Trace:
     def __init__(self, name, duration, t_start, in_dep):
